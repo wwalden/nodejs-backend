32712f97032de9077c722b8ab1d15b9c0bff226f
Arnaud <74240972+withwalden@users.noreply.github.com>
2021-11-08 15:47:57 +0100

    Nov8_commit_v1

--- back/app.js
+++ back/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res) => {
-   res.json({ message: 'watashi wa backend!' }); 
+   res.json({ message: 'watashi wa el backend!' }); 
 });
 
 module.exports = app;
