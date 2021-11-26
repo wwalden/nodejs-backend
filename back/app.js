a2480a9e41e74df04f434da0461f5648c2c28192
Arnaud <74240972+withwalden@users.noreply.github.com>
2021-11-26 12:28:46 +0100

    cleaning code

--- back/app.js
+++ back/app.js
@@ -28,35 +28,3 @@ app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-
-//const helmet = require("helmet");
-//const nocache = require('nocache');
-
-
-//app.use(helmet());
-//app.use(nocache());
-
-
-*/
